912a75720309dcc5141c747d4e56a2886b3161c8
clayajohnson <johnson.clayton.a@gmail.com>
2020-06-30 00:08:23 +1000

    detect blue line and render in real time

--- betterCar.py
+++ betterCar.py
@@ -1,15 +1,20 @@
 import simulatorInterface
 import cv2
+import numpy as np
 
 def controller(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    cv2.imshow("car view", blur)
+    imcopy = np.copy(image)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv_mask = cv2.inRange(hsv,(100,60,100),(115,255,255))
+    contours,hierarchy = cv2.findContours(hsv_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+    cv2.drawContours(imcopy,contours,-1,(0,255,0),1)
+    cv2.imshow("result",imcopy)
     cv2.waitKey(1)
 
     controlCommand={}
     controlCommand["speed"]=100
-    controlCommand["steer"]=-0.5
+    controlCommand["steer"]=0
     return controlCommand
 
 simulatorInterface.onMessage(controller)
