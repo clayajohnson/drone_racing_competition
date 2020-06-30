cc90290dc19ee2d6d878c50c1931264826a87d7d
hussam123-lab <62464644+hussam123-lab@users.noreply.github.com>
2020-06-30 20:44:32 +1000

    Update sampleCar.py

--- sampleCar.py
+++ sampleCar.py
@@ -1,29 +1,22 @@
-import simulatorInterface
-import cv2
-import numpy as np
-import math
-import time
-
-
-
 def controller(image):
     #cv2.imshow("car view",image)
     #cv2.waitKey(1)
 
 
-    #hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     #ret,thresh = cv2.threshold(hsv_image,127,100,0)
     #contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     #print(len(contours))
-    upper_blue = np.array([230,73,100])
-    lower_blue = np.array([216,100,59])
+    upper_blue = np.array([500,300,400])
+    #lower_blue = np.array([150,150,20])
+    lower_blue = np.array([50,50,0])
     #ret,thresh = cv2.threshold(hs)
 
-    #mask = cv2.inRange(hsv_image, upper_blue, lower_blue)
+    mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
     #_,contour,_ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     #cv2.drawContours(image,contour,-1,(0,255,0),3)
 
-    #res = cv2.bitwise_and(image,image,mask=mask)
+    res = cv2.bitwise_and(image,image,mask=mask)
     h, w, _ = image.shape
     flag = False
     x = 0
@@ -41,28 +34,31 @@ def controller(image):
             if res[i][j][2] >0:
                 x = i
                 y = j
-    
                 flag = True
                 break
         if flag == True:
             break
 
     z = math.sqrt(((240 - h)**2) + ((320 - y)**2))
-    #print(z)
+    print(z)
 
-    if z <= 400:
+    if z <= 200:
         controlCommand["steer"]= 5
-        controlCommand["speed"]=10
-
+        controlCommand["speed"]=1000
     else:
-        controlCommand["steer"]= 0
+        controlCommand["steer"]= 100
+        controlCommand["speed"]=1000
+
+
+    #else:
+    #    controlCommand["steer"]= 0
 
 
 
 
 
 
-    cv2.imshow("car view",image)
+    cv2.imshow("car view",res)
     cv2.waitKey(1)
 
 
