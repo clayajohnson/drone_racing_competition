c6f4ea15a421c57ab47ab220ff7e41e80ddc5ee7
clayajohnson <johnson.clayton.a@gmail.com>
2020-06-30 20:55:46 +1000

    added algo to calculate distance from car to edges, doesn't work yet

--- betterCar.py
+++ betterCar.py
@@ -16,7 +16,32 @@ def controller(image):
     # find contours (edges)
     blue_contours,hierarchy = cv2.findContours(blue_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     yellow_contours,hierarchy = cv2.findContours(yellow_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-
+    # # find the main blue contour
+    # if blue_contours:
+    #     main_blue_contour = None
+    #     lowest_pt = 0
+    #     for contour in blue_contours:
+    #         if len(contour) < 50:
+    #             continue
+    #         this_lowest_index = np.argmax(contour[:,0,1])
+    #         this_lowest_pt = contour[this_lowest_index,0,1]
+    #         if this_lowest_pt > lowest_pt:
+    #             lowest_pt = this_lowest_pt
+    #             main_blue_contour = contour
+    #     #cv2.circle(view,(main_blue_contour[lowest_pt][0][0],main_blue_contour[lowest_pt][0][1]),4,(255,0,255))
+    # # find the main yellow contour
+    # if yellow_contours:
+    #     main_yellow_contour = None
+    #     lowest_pt = 0
+    #     for contour in yellow_contours:
+    #         if len(contour) < 50:
+    #             continue
+    #         this_lowest_index = np.argmax(contour[:,0,1])
+    #         this_lowest_pt = contour[this_lowest_index,0,1]
+    #         if this_lowest_pt > lowest_pt:
+    #             lowest_pt = this_lowest_pt
+    #             main_yellow_contour = contour
+    #     #cv2.circle(view,(main_yellow_contour[lowest_pt][0][0],main_yellow_contour[lowest_pt][0][1]),4,(255,0,255))
     # draw countours (green) on view
     cv2.drawContours(view,blue_contours,-1,(0,255,0),1)
     cv2.drawContours(view,yellow_contours,-1,(0,255,0),1)
